fdaa7d6d36e1c228925457e0c6ff17a696d71e3a
Benjamín Peralta <benjaminp158@gmail.com>
2026-02-15 17:46:35 -0300

    fix: added required to description and amount

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ if __name__ == "__main__":
     argparser: argparse.ArgumentParser = argparse.ArgumentParser(
         "expense tracker", description="tracker de gastos"
     )
-    subparsers = argparser.add_subparsers(dest="acción", required=True)
+    subparsers = argparser.add_subparsers(dest="action", required=True)
     add_parser = subparsers.add_parser("add")
-    add_parser.add_argument("--description", "-d")
-    add_parser.add_argument("--amount", "-a", type=int)
-
+    add_parser.add_argument("--description", "-d", required=True)
+    add_parser.add_argument("--amount", "-a", type=int, required=True)
     delete_parser = subparsers.add_parser("delete")
     delete_parser.add_argument("--id", required=True, type=int)
 
