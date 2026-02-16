db653765b003987eeccd4edefa07b068e7db94cf
Benjamín Peralta <benjaminp158@gmail.com>
2026-02-16 11:02:14 -0300

    feat: added ExpenseRecord dataclass

---
+++ ExpenseRecord.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass, field
+from datetime import datetime, date as Date
+
+
+@dataclass
+class ExpenseRecord:
+    amount: int
+    description: str
+    date: Date = field(default=Date.today())
